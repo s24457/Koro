82f207e33adc9f87f5a95b38751ba74b013ef086
s24457 <92578049+s24457@users.noreply.github.com>
2021-10-29 14:15:34 +0200

    Create s01-sub-stdin.cpp

---
+++ s01-sub-stdin.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+using namespace std;
+
+auto ask_user_for_int(string prompt)-> int {
+    cout << prompt << "int: ";
+    auto n = string {};
+    getline(cin, n);
+    return stoi(n);
+}
+
+auto main () -> int {
+    auto const a = ask_user_for_int("a= ");
+    auto const b = ask_user_for_int("b= ");
+    cout << a - b << endl;
+    return 0;
+}
